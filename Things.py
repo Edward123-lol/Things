57cdbefa8bd7f43b7ed8d1b9a7747786f5b77fd1
100128112 <edward05.jang@gmail.com>
2019-10-24 15:35:37 -0500

    Learning about strings

---
+++ Things.py
@@ -0,0 +1,29 @@
+# Strings
+
+# Concatenation
+#    2 or more strings and put them together
+
+firstName = "Wilma"
+lastName = "Flintstone"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+#   repetition operator: *
+
+print("Hip " * 2 + "Hooray!")
+
+
+def rowYourBoat():
+    print("Row, " * 3 + "your boat")
+    print("Gently down the stream")
+    print("Merrily, " * 4)
+    print("Life is but a dream")
+
+
+rowYourBoat()
